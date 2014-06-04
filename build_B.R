13c81054690e372d7f432cf13cd438b6d0c234f0
nickalv10094 <npa02012@mymail.pomona.edu>
2014-06-04 10:47:24 -0700

    Notes added to Build.B
    Notes added
    2nd line (initial B creation) is changed: B <-matrix(0,...)
    Many lines shifted over two spaces to line up properly

--- build_B.R
+++ build_B.R
@@ -1,42 +1,45 @@
-build.B <- function(k,p,q, Btype){
-  B <- matrix(rep(0,p*q),nrow = p, ncol = q)
+build.B <- function(k,p,q, Btype){	#Creates pxq diagonal block matrix. Block elements are k or 0.
+					#Defines relationship between X and Y. E(Y) = B*X
+
+  B <- matrix(0,nrow = p, ncol = q)	#Creates the initial pxq matrix, all elements 0.
   
   if(Btype==0){
     for(i in 1:2){
       for(j in 1:3){
-        B[i,j] = k
-      }
-    }
-    for(i in 3:5){
-      for(j in 4:5){
-        B[i,j] = k
-      }
-    }
-    
-    
-  } 
-    
-    if (Btype==1){
-      for(i in 1:12){
-        for(j in 1:8){
-          B[i,j] <- k
-        }
-      }
-      for(i in 13:20){
-        for(j in 9:12){
-          B[i,j] <- k
-        }
-      }
-      for(i in 21:32){
-        for(j in 13:16){
-          B[i,j] <- k
-        }
+        B[i,j] = k	#Rows 1-2, Collumns 1-3, entries of B = k.
       }
     }
 
+    for(i in 3:5){
+      for(j in 4:5){
+        B[i,j] = k	#Rows 3-5, Collumns 4-5, entries of B = k.
+      }
+    }
+  } 
+    
+  if (Btype==1){
+    for(i in 1:12){
+      for(j in 1:8){
+        B[i,j] <- k	#Rows 1-12, Collumns 1-8, entries of B = k.
+      }
+    }
+
+    for(i in 13:20){
+      for(j in 9:12){
+        B[i,j] <- k	#Rows 13-20, Collumns 9-12, " ".
+      }
+    }
+
+    for(i in 21:32){
+      for(j in 13:16){
+        B[i,j] <- k	#Rows 21-32, Collumns 13-16, " ".
+      }
+    }
+  }
+
 # for Btype == 2 we need to think about a bigger matrix to test
     
-    if(Btype==2)  {}
+  if(Btype==2)  {}
     
-    return(B)
-  }
+  return(B)
+  }	
