18ebd162775094bf6e427a1c6802e419d589ba83
nickalv10094 <npa02012@mymail.pomona.edu>
2014-06-20 12:02:22 -0700

    Cov_suped.R final commit
    Notes added
    Indentation fixed
    In building initial D.XX, matrix arguments of bdiag() have cor.level as
    first argument, as opposed to a vector.
    diag(D.XX) <- rep(1,matdim) changed to <- 1
    G changed to X

--- Cov_suped.R
+++ Cov_suped.R
@@ -1,49 +1,61 @@
+## Cov.suped returns a (matdim x matdim) covariance matrix for X or Y. ##
+
+
 Cov.suped <- function(matdim, cor.level, which.matrix, Btype=1, SDX=5){
+# Called by sim.setup.noise.suped()
+# Calls no other rmscca functions
 
-  library(Matrix)
-    
+library(Matrix)
 
-# for each Btype, the variables within a complete group are correlated (as we would expect them to be)
+
+# Create initial D.XX based on "Btype" and "which.matrix".
+# D.XX is a (matdim x matdim) diagonal block matrix. Initially, all non-zero entries equal cor.level.
+# For each Btype, the variables within a complete group are correlated (as we would expect them to be)
+#  For example, if x1 and x2 are in the same block of B, x1 and x2 will be correlated within the simulated data.
 
 if(Btype==0){
-  if(which.matrix=='G'){
-  D.XX <- as.matrix(bdiag(matrix(rep(cor.level,4),2),matrix(rep(cor.level,9),3),
-		matrix(rep(0,(matdim-2-3)^2),(matdim-2-3))))
+  if(which.matrix=='X'){
+    D.XX <- as.matrix(bdiag(matrix(cor.level,2,2), matrix(cor.level,3,3), matrix(0,matdim-2-3,matdim-2-3)))
   }else{
-    D.XX <- as.matrix(bdiag(matrix(rep(cor.level,9),3),matrix(rep(cor.level,4),2),
-		matrix(rep(0,(matdim-3-2)^2),(matdim-3-2))))
+    D.XX <- as.matrix(bdiag(matrix(cor.level,3,3), matrix(cor.level,2,2), matrix(0,matdim-3-2,matdim-3-2)))
   }
-  }
-if(Btype==1){
-  if(which.matrix=='G'){
-  D.XX <- as.matrix(bdiag(matrix(rep(cor.level,144),12),matrix(rep(cor.level,64),8), matrix(rep(cor.level,144),12),
-		matrix(rep(0,(matdim-8-12-12)^2),(matdim-8-12-12))))
-  }else{
-    D.XX <- as.matrix(bdiag(matrix(rep(cor.level,64),8),matrix(rep(cor.level,16),4), matrix(rep(cor.level,16),4),
+}
 
-		matrix(rep(0,(matdim-8-4-4)^2),(matdim-8-4-4))))
+
+if(Btype==1){
+  if(which.matrix=='X'){
+    D.XX <- as.matrix(bdiag(matrix(cor.level,12,12), matrix(cor.level,8,8), matrix(cor.level,12,12), 	
+	  	 	 matrix(0,matdim-8-12-12,matdim-8-12-12)))
+  }else{
+    D.XX <- as.matrix(bdiag(matrix(cor.level,8,8), matrix(cor.level,4,4), matrix(cor.level,4,4),
+			matrix(0,matdim-8-4-4,matdim-8-4-4)))	
   }
-  }
+}
+
 
 if(Btype==2){  # needs to be fixed
-  if(which.matrix=='G'){
-  D.XX <- as.matrix(bdiag(matrix(rep(cor.level,144),12),matrix(rep(cor.level,64),8), matrix(rep(cor.level,144),12),
-		matrix(rep(0,(matdim-8-12-12)^2),(matdim-8-12-12))))
+  if(which.matrix=='X'){
+    D.XX <- as.matrix(bdiag(matrix(cor.level,12,12), matrix(cor.level,8,8), matrix(cor.level,12,12),
+	         	matrix(0,matdim-8-12-12,matdim-8-12-12)))
   }else{
-    D.XX <- as.matrix(bdiag(matrix(rep(cor.level,64),8),matrix(rep(cor.level,16),4), matrix(rep(cor.level,16),4),
-		matrix(rep(0,(matdim-8-4-4)^2),(matdim-8-4-4))))
+    D.XX <- as.matrix(bdiag(matrix(cor.level,8,8), matrix(cor.level,4,4), matrix(cor.level,4,4),
+			matrix(0,matdim-8-4-4,matdim-8-4-4)))
   }
-  }
+}
+  
+diag(D.XX) <- 1		#Each entry of diagonal of D.XX set equal to 1.
+
+# D.XX now created
 
   
-  diag(D.XX) <- rep(1,matdim)
-    
-  #   Pre- and post- multiply the whole thing by a st dev vector to get a 
-  #   covariandce matrix instead of a correlation matrix.
-  
-  sd.XX = rnorm(matdim, SDX, 1)  
-  cor.XX = D.XX
+sd.XX = rnorm(matdim, SDX, 1)	#(matdim x 1) standard deviation vector
+
 
 # I don’t know how you want to come up with the variances on the diagonal…  Make sure they are all positive.
-  
-  return(cov.XX = t( cor.XX *  sd.XX) * sd.XX)}
+
+# Pre- and post- multiply D.XX by a standard deviation vector to get a 
+#  covariance matrix instead of a correlation matrix.
+
+return(cov.XX = t( D.XX *  sd.XX) * sd.XX)
+
+}
