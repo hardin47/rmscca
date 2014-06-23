0f8d88d8832a5e01af3ada0abdd6448dbb9d76aa
nickalv10094 <npa02012@mymail.pomona.edu>
2014-06-22 18:04:29 -0700

    sample.sigma12.function Commit
    Indentation changed
    Notes added
    Method of subtraction of column means from X and Y changed

--- sample.sigma12.function.R
+++ sample.sigma12.function.R
@@ -1,41 +1,49 @@
-sample.sigma12.function <- function(x, y, robust = T)
-{
+## Takes X and Y data  and estimates the covariance matrix. ##
+
+sample.sigma12.function <- function(X, Y, robust = T) {
+# Takes X and Y data  and estimates the covariance matrix.
+# Called by scca.multiple & select.parameters.multiple.R
+# Calls no other rmscca functions
 
 # Jake originally had these two lines in the code, but I don't think we want to hard-code
 # the training samples
 #  x = x.data[training.sample,]
 #  y = y.data[training.sample,]
   
-  x <- t(t(x) - apply(x,2,mean))
-  y <- t(t(y) - apply(y,2,mean))
+# Subtract column (variable) means
+X <- X - colMeans(X)
+Y <- Y - colMeans(Y)
   
-  #M-estimation if robust method desired
-  #     Big = cbind(x,y)
-  #     r.cov = CovMest(Big)@cov
-  #     dimx = dim(x)[2]
-  #     dimy = dim(y)[2]
-  #     dims = dimy+dimx
-  #     Sigma11 <- diag(1/sqrt(diag(r.cov[1:dimx,1:dimx])))
-  #     
-  #     Sigma22 <- diag(1/sqrt(diag(r.cov[(dimx+1):dims, (dimx+1):dims])))
-  #     Sigma12 <- r.cov[1:dimx,(dimx+1):dims]
-  #     
-  #     return(Sigma11%*%Sigma12%*%Sigma22)
+# M-estimation if robust method desired
+#     Big = cbind(x,y)
+#     r.cov = CovMest(Big)@cov
+#     dimx = dim(x)[2]
+#     dimy = dim(y)[2]
+#     dims = dimy+dimx
+#     Sigma11 <- diag(1/sqrt(diag(r.cov[1:dimx,1:dimx])))
+#     
+#     Sigma22 <- diag(1/sqrt(diag(r.cov[(dimx+1):dims, (dimx+1):dims])))
+#     Sigma12 <- r.cov[1:dimx,(dimx+1):dims]
+#     
+#     return(Sigma11%*%Sigma12%*%Sigma22)
   
-  #Method From Parkhomenko
-  # Sample variance-covariance matrices 
-  sigma11 <- var(x)
-  sigma22 <- var(y)
+# Method From Parkhomenko
+# Sample variance-covariance matrices 
+sigma11 <- var(X)
+sigma22 <- var(Y)
 
-  diag(sigma11) = ifelse(diag(sigma11)==0,1,diag(sigma11))
-  diag(sigma22) = ifelse(diag(sigma22)==0,1,diag(sigma22))
+# Change any 0 diagonal entries to 1
+diag(sigma11) = ifelse(diag(sigma11)==0, 1, diag(sigma11))
+diag(sigma22) = ifelse(diag(sigma22)==0, 1, diag(sigma22))
   
-  x <- x %*% diag( 1/sqrt(diag(sigma11)) )
-  y <- y %*% diag( 1/sqrt(diag(sigma22)) )
+X <- X %*% diag( 1/sqrt(diag(sigma11)) )	#Multiply X by normalized diagonal vector of sigma11
+Y <- Y %*% diag( 1/sqrt(diag(sigma22)) )	#Multiply Y by normalized diagonal vector of sigma22
   
-  if(robust == T){
-    return(cov(x,y, method = "spearman"))
-  }else{
-    return(cov(x,y,method = "pearson"))
-  }
+if(robust == T){
+  return(cov(X,Y, method = "spearman"))
+
+}else{
+  return(cov(X,Y,method = "pearson"))
+}
+
 }
