74cdf472847b59fdf9a9773e6db28cc9ddbade83
hardin47 <jo.hardin@pomona.edu>
2014-06-23 10:07:36 -0700

    to run more quickly
    changed the maximum number of iterations and convergence criteria to try
    to run the function more quickly.

--- scca.function.R
+++ scca.function.R
@@ -1,15 +1,15 @@
 ## Runs SCCA for one canonical pair ##
 
 scca.function <- function(k, u.initial, v.initial, lambda.u, lambda.v,gamma = 1) {
+
 # Called by scca.multiple.R & select.multiple.parameters.R
 # Calls no other rmscca functions
 
 i <- 0 		  # Number of iterations used by SCCA
-eps <- 0.001	  # Convergence criterion 
-max.iter <- 50	  # Maximum nuber of iterations
+eps <- 0.01	  # Convergence criterion (was 0.001)
+max.iter <- 20	  # Maximum nuber of iterations (was 50)
 diff.u <- eps*10	
 diff.v <- eps*10
-# svdvec <- svd(k,nu = 1, nv = 1)
 
 while ((i < max.iter) & ((diff.u > eps) || (diff.v > eps)) ) {
 
@@ -18,18 +18,17 @@ while ((i < max.iter) & ((diff.u > eps) || (diff.v > eps)) ) {
   # Update left singular vector
   vx <-  k %*% v.initial
 
-  length.vx <- as.numeric(sqrt(t(vx)%*%vx))		#Normalize vx
+  length.vx <- as.numeric(sqrt(t(vx)%*%vx))		# Normalize vx
   if(length.vx==0) length.vx <- 1			# 
   vx <- vx / length.vx					#
 
- #   svdu <- norm(abs(as.matrix(svdvec$u[,1])^gamma),"F")
 
-  u.new <- abs(vx) - 0.5*lambda.u  #/svdu 		#Apply soft-thresholding to
+  u.new <- abs(vx) - 0.5*lambda.u  #/svdu 		# Apply soft-thresholding to
   u.new <- (u.new + abs(u.new))/2			# obtain u.new, a
-  u.new <- u.new*sign(vx)				#  sparse solution
+  u.new <- u.new*sign(vx)				# sparse solution
 
 
-  length.u.new <- as.numeric(sqrt(t(u.new)%*%u.new))	#Normalize u.new
+  length.u.new <- as.numeric(sqrt(t(u.new)%*%u.new))	# Normalize u.new
   if(length.u.new==0) length.u.new <- 1			#
   u.new <- u.new / length.u.new				#
     
@@ -37,28 +36,27 @@ while ((i < max.iter) & ((diff.u > eps) || (diff.v > eps)) ) {
   # Update right singular vector
   ux <- t(k) %*% u.new
 
-  length.ux <- as.numeric(sqrt(t(ux)%*%ux))		#Normalize ux
+  length.ux <- as.numeric(sqrt(t(ux)%*%ux))		# Normalize ux
   if(length.ux==0) length.ux <- 1			#  
   ux <- ux / length.ux					#
 
- #   svdv <- norm(abs(as.matrix(svdvec$v[,1])^gamma),"F")
 
-  v.new <- abs(ux) - 0.5*lambda.v  #/svdvq		#Apply soft-thresholding to
+  v.new <- abs(ux) - 0.5*lambda.v  #/svdvq		# Apply soft-thresholding to
   v.new <- (v.new + abs(v.new))/2			# obtain v.new, a
-  v.new <- v.new * sign(ux)				#  sparse solution
+  v.new <- v.new * sign(ux)				# sparse solution
 
 
-  length.v.new <- as.numeric(sqrt(t(v.new)%*%v.new))	#Normalize v.new
+  length.v.new <- as.numeric(sqrt(t(v.new)%*%v.new))	# Normalize v.new
   if(length.v.new==0) length.v.new <- 1			#
   v.new <- v.new / length.v.new				#
     
     
   # Convergence measures  
-  diff.v <- max(abs(v.initial - v.new))			#Update diff.v
-  diff.u <- max(abs(u.initial - u.new))			#Update diff.u
+  diff.v <- max(abs(v.initial - v.new))			# Update diff.v
+  diff.u <- max(abs(u.initial - u.new))			# Update diff.u
     
-  v.initial <- v.new					#Update v.initial
-  u.initial <- u.new					#Update u.initial
+  v.initial <- v.new					# Update v.initial
+  u.initial <- u.new					# Update u.initial
 
   } # Close while loop
   
