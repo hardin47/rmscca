75da5d98f94710db4637af9b69787ffdb0b91961
hardin47 <jo.hardin@pomona.edu>
2014-06-23 11:04:18 -0700

    update simulations
    For clean, t, and symmetric noise, I did the following:
    * no lists
    * mu from matrix multiplication
    * no G

--- sim_setup_noise_suped.R
+++ sim_setup_noise_suped.R
@@ -1,100 +1,86 @@
 ## Simulates the data to test ##
 
 sim.setup.noise.suped <- function(n.obs, B, contamination, var.cor, noisetype, Btype=1, SDX=1){
+
 # Called by big_sim_cutoff.R
 # Calls Cov.suped
+# source("Final_funcs/Cov_suped.R")
   
-source("Final_funcs/Cov_suped.R")
-  
-p <- dim(B)[1]
-q <- dim(B)[2]
+p = dim(B)[1]
+q = dim(B)[2]
   
 if(noisetype == "clean"){
-  Sigma.g = Cov.suped(p,cor.level = var.cor, which.matrix = 'G', Btype, SDX)	#Create X covariance matrix
+  Sigma.x = Cov.suped(p,cor.level = var.cor, which.matrix = 'X', Btype, SDX)	#Create X covariance matrix
+  Sigma.y = Cov.suped(q, cor.level = var.cor, which.matrix = 'Y',Btype, SDX)	#Create Y covariance matrix
   
-  G <- mvrnorm(n.obs,rep(0,p), Sigma.g)		#Produces n.obs samples from multivariate distibrution
-						# with means 0 and covariance matrix Sigma.g
-  mu <- matrix(0,nrow=n.obs,ncol=q)
-  mu <- G%*%B
-  
-  # Need to find reasonable parameters for this.
-  Sigma.y <- Cov.suped(q, cor.level = var.cor, which.matrix = 'Y',Btype, SDX)	#Create Y covariance matrix
+  # Produces n.obs samples from multivariate normal distibrution with mean 0 and covariance matrix Sigma.x
+  X = mvrnorm(n.obs,rep(0,p), Sigma.x)		
+  mu = X%*%B
 
-  Y.pheno <-list()
-  length(Y.pheno) <-q
-
+  Y.pheno = matrix(ncol=q, nrow=n.obs)
   for(i in 1:n.obs){
-      Y.pheno[[i]] <- mvrnorm(1,mu[i,], Sigma.y)
-    }
-  Y.pheno <- matrix(unlist(Y.pheno),nrow = n.obs, byrow =T)
-  return(list(X = G, Y = Y.pheno))
+  	Y.pheno[i,] = mvrnorm(1, mu[i,], Sigma.y)
+  }
+
+  return(list(X = X, Y = Y.pheno))
 }
 
 
 
 if(noisetype=="t"){
 
-  tdf = 1
-  Sigma.g = Cov.suped(p, cor.level = var.cor, which.matrix = 'G', Btype, SDX)
+  tdf = 2
+  Sigma.x = Cov.suped(p,cor.level = var.cor, which.matrix = 'X', Btype, SDX)	#Create X covariance matrix
+  Sigma.y = Cov.suped(q, cor.level = var.cor, which.matrix = 'Y',Btype, SDX)	#Create Y covariance matrix
+  
+  # Produces n.obs samples from multivariate t-distibrution with mean 0 and covariance matrix Sigma.x
+  X = mvrnorm(n.obs,rep(0,p), Sigma.x)/sqrt(rchisq(n.obs,tdf)/tdf)	
+  mu = X%*%B
 
-  G <- mvrnorm(n.obs,rep(0,p), Sigma.g)/sqrt(rchisq(n.obs,tdf)/tdf)
-  mu <- matrix(rep(0,n.obs*q),nrow=n.obs,ncol=q)
-  for (i in 1:n.obs){
-    mu[i,] <- G[i,]%*%B
+  Y.pheno = matrix(ncol=q, nrow=n.obs)
+  for(i in 1:n.obs){
+  	Y.pheno[i,] = mvrnorm(1, mu[i,], Sigma.y)/sqrt(rchisq(1,tdf)/tdf)
   }
 
-  #need to find reasonable parameters for this
-  Sigma.y <- Cov.suped(matdim = q, cor.level = var.cor, which.matrix = 'Y', Btype, SDX)
-
-  Y.pheno <-list()
-  length(Y.pheno) <-q
-  for(i in 1:n.obs){
-      Y.pheno[[i]] <- mvrnorm(1,mu[i,], Sigma.y)/sqrt(rchisq(q,tdf)/tdf)
-    }
-  Y.pheno <- matrix(unlist(Y.pheno),nrow = n.obs, byrow =T)
-  return(list(X = G, Y = Y.pheno))
-
+  return(list(X = X, Y = Y.pheno))
 }
+
 
 
 if(noisetype=="sym"){
 
-  Sigma.g = Cov.suped(p, cor.level = var.cor, which.matrix = 'G', Btype, SDX)
+  Sigma.x = Cov.suped(p, cor.level = var.cor, which.matrix = 'X', Btype, SDX)
+  Sigma.y = Cov.suped(matdim = q, cor.level = var.cor, which.matrix = 'Y', Btype, SDX)
 
-  #need to find reasonable parameters for this
-  Sigma.y <- Cov.suped(matdim = q, cor.level = var.cor, which.matrix = 'Y', Btype, SDX)
+  X = mvrnorm(n.obs, rep(0,p), Sigma.x)
+  mu = X %*%B
 
-  G.clean = mvrnorm(n.obs, rep(0,p), Sigma.g)
+  Y.pheno = matrix(ncol=q, nrow=n.obs)
+  for(i in 1:n.obs){
+  	Y.pheno[i,] = mvrnorm(1, mu[i,], Sigma.y)
+  }
 
-  Y.pheno = G.clean%*%B
-
-#  mu = G.clean %*%B
-#  Y.pheno = matrix(ncol=q, nrow=n.obs)
-#  for(i in 1:n.obs){
-#  	Y.pheno[i,] = mvrnorm(1, mu[i,], Sigma.y)
-#	}
-
-  mins.G = min(apply(G.clean,1,min))
-  maxes.G = max(apply(G.clean,1,max))
+  mins.X = min(apply(X,1,min))
+  maxes.X = max(apply(X,1,max))
   mins.Y = min(apply(Y.pheno,1,min))
   maxes.Y = max(apply(Y.pheno,1,max))
 
   for(i in 1:n.obs){
   	if(runif(1) < contamination){
-		    G.clean[i,] = runif(p, 3*mins.G, 3*maxes.G)
+		    X[i,] = runif(p, 3*mins.G, 3*maxes.G)
 		    Y.pheno[i,] = runif(q, 3*mins.Y, 3*maxes.Y)
 		    }
 	}
 
 
-  return(list(X = G.clean, Y = Y.pheno))
+  return(list(X = X, Y = Y.pheno))
 
 }
 
 
 if(noisetype=="asym"){
 
-  Sigma.g = Cov.suped(p,  cor.level = var.cor, which.matrix = 'G', Btype, SDX)
+  Sigma.g = Cov.suped(p,  cor.level = var.cor, which.matrix = 'X', Btype, SDX)
 
   num.noise <- n.obs*contamination
   num.clean <- n.obs*(1-contamination)
